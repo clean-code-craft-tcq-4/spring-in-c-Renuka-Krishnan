7585d7cd2ef8350269a65573f0ce68ec838b27b6
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-05-22 21:24:58 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,6 +5,29 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int i=0;
+    float Total = 0;
+    if(setlength == 0)
+    {
+        s.average = NAN;
+        s.max = NAN;
+        s.min = NAN;
+    }
+    else
+    {
+     for(i=0,i<setlength,i++)
+         {
+          if(numberset[i] < s.min)
+          s.min= numberset[i];
+
+          if(numberset[i] > s.max )
+          s.max = numberset[i];  
+         
+         Total = Total + numberset[i];
+         }
+       s.average =  Total/setlength;
+    }
+    return s;
 }
 
 int emailAlertCallCount = 0;
