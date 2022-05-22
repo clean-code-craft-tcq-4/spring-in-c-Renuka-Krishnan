5182e6090f827336375050c40bca8132483293c8
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-05-22 22:58:51 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,6 +2,19 @@
 #include "math.h"
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+
+void emailAlerter()
+{
+     emailAlertCallCount++;
+}
+void ledAlerter()
+{
+    ledAlerterCallCount++;
+}
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
+  alerters[0]();
+  alerters[1]();
+}
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
