fe208c251c64f441595dee0ae092c039d17b5bb9
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-05-22 23:29:16 +0530

    Update emailAlerter.c

--- emailAlerter.c
+++ emailAlerter.c
@@ -1,5 +1,11 @@
 #include "emailAlerter.h"
 
+
+/*
+Function name : emailAlerter
+Formal parameters : void
+Function Description : This function increments the emailAlertCallCount on each call.
+*/
 void emailAlerter()
 {
      emailAlertCallCount++;
