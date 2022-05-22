d18cbf4cbf158d37e992ada9e7ca27387e26e710
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-05-22 23:29:56 +0530

    Update ledAlerter.c

--- ledAlerter.c
+++ ledAlerter.c
@@ -1,5 +1,10 @@
 #include "ledAlerter.h"
 
+/*
+Function name : ledAlerter
+Formal parameters : void
+Function Description : This function increments the ledAlertCallCount on each call.
+*/
 void ledAlerter()
 {
     ledAlertCallCount++;
